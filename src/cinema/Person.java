f6f8531ed2e218a3909198170df6bbb02342dff7
Carolina <marielachinchilla1509@gmail.com>
2025-10-11 09:35:57 -0600

    Codificación de clases. Person, Customer, Ticket

--- src/cinema/Person.java
+++ src/cinema/Person.java
@@ -15,4 +15,86 @@ public class Person {
     private String name;
 
     private String phoneNumber;
+
+    public Person(boolean disability, Date birthDay, String email, String id,
+            String name, String phoneNumber) {
+        this.disability = disability;
+        this.birthDay = birthDay;
+        this.email = email;
+        this.id = id;
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public Person() {
+        boolean disability= false;
+        Date birthday = null;
+        String email= "Not registered";
+        String id= "Not registered";
+        String name= "Not registered";
+        String phoneNumber= "Not registered";
+    }
+
+    public boolean isDisability() {
+        return disability;
+    }
+
+    public Date getBirthDay() {
+        return birthDay;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setDisability(boolean disability) {
+        this.disability = disability;
+    }
+
+    public void setBirthDay(Date birthDay) {
+        this.birthDay = birthDay;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" + "disability=" + disability + ", birthDay=" +
+                birthDay + ", email=" + email + ", id=" + id + ", name=" +
+                name + ", phoneNumber=" + phoneNumber + '}';
+    }
+    
+    
+    
+
+    
+    
+    
+    
 }
